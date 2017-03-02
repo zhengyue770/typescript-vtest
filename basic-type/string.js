2f11cfd70a6bd83ad72bcedc6b176363ad222f66
zhengyue770 <zhengyue770@users.noreply.github.com>
2017-03-02 17:33:54 +0800

    Delete string.js

--- basic-type/string.js
+++
@@ -1,8 +0,0 @@
-/**
- * Created by Amaris on 2017/3/2.
- */
-//多行文本与内嵌表达式
-var name1 = "Gene";
-var age = 37;
-var sentence = "Hello, my name is " + name1 + ".\n\nI'll be " + (age + 1) + " years old next month.";
-document.body.innerHTML = sentence;
